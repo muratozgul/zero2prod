b73376c47968ab99772c339b82c1320ab3505977
muratozgul <mur.ozgul@gmail.com>
2024-09-18 23:27:01 +0100

    4.5.11

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::config::get_config;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let config = get_config().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&config.database.connection_string())
